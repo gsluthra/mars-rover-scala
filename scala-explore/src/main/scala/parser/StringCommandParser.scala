9a68de47c9e13e3170b2cd4ea1b3f9ba6a1565a1
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-09-28 16:52:34 +0530

    Small formatting of code

--- scala-explore/src/main/scala/parser/StringCommandParser.scala
+++ scala-explore/src/main/scala/parser/StringCommandParser.scala
@@ -4,7 +4,7 @@ import commands.{TurnRightCommand, TurnLeftCommand, MoveCommand, RoverCommand}
 
 case class StringCommandParser(commandString: Option[String]){
   def commands: Seq[RoverCommand] = {
-     commandString.getOrElse("").map{
+     commandString.getOrElse("").map {
        case 'M' => MoveCommand
        case 'L' => TurnLeftCommand
        case 'R' => TurnRightCommand
--- scala-explore/src/main/scala/rover/MarsRover.scala
+++ scala-explore/src/main/scala/rover/MarsRover.scala
@@ -1,12 +1,12 @@
 package rover
 
-import commands.RoverCommand
 import parser.StringCommandParser
 import universe._
 
-case class MarsRover(plateau: Plateau, currentPoint: Coordinates, face: Direction){
+case class MarsRover(plateau: Plateau, currentPoint: Coordinates, face: Direction) {
+
   def run(commandString: String) = {
-    val commands: Seq[RoverCommand] = StringCommandParser(Option(commandString)).commands
+    val commands = StringCommandParser(Option(commandString)).commands
     commands.foldLeft(this)((rover, command) => command.execute(rover))
   }
 
@@ -15,32 +15,23 @@ case class MarsRover(plateau: Plateau, currentPoint: Coordinates, face: Directio
     val newCoordinate = face match {
       case NORTH => currentPoint.incrementBy(0, 1)
       case SOUTH => currentPoint.incrementBy(0, -1)
-      case EAST  => currentPoint.incrementBy( 1, 0)
+      case EAST  => currentPoint.incrementBy(1, 0)
       case WEST  => currentPoint.incrementBy(-1, 0)
     }
 
-    if(plateau.withinBounds(newCoordinate))
-      MarsRover(plateau, newCoordinate , face)
+    if (plateau.withinBounds(newCoordinate))
+      MarsRover(plateau, newCoordinate, face)
     else
       this
-
   }
 
 
-  def turnLeft() = {
-      MarsRover(plateau, currentPoint, face.left)
-  }
+  def turnLeft() = MarsRover(plateau, currentPoint, face.left)
 
-  def turnRight() = {
-      MarsRover(plateau, currentPoint, face.right)
-  }
+  def turnRight() = MarsRover(plateau, currentPoint, face.right)
 
-  def currentLocation: String = {
-    toString
-  }
+  def currentLocation: String = toString
 
-  override def toString: String = {
-    s"(${currentPoint.x}, ${currentPoint.y}, ${face})"
-  }
+  override def toString: String = s"(${currentPoint.x}, ${currentPoint.y}, ${face})"
 
 }
